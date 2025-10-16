ec89fa66cd3eb298a7fd5675c8ae234122ac5249
Raphael Araújo <raphox.araujo@gmail.com>
2025-10-15 18:01:36 -0600

    feat: add Firebase configuration files and deployment workflows

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,7 +13,7 @@ import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
 
 export const unstable_settings = {
-  anchor: "(home)/index",
+  anchor: "(home)",
 };
 
 export default function RootLayout() {
